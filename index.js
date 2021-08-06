2b5a8f3dd46aa56a74ad5c63b0773749821f03e0
adelina2222 <adelinachiriac@gmail.com>
2021-08-06 11:05:54 +0100

    Hide the message form & reveal the link (after form submission)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
 
+    // hide the secret message input form
+    document.querySelector("#message-form").classList.add("hide");
+    // reveal the link input
+    document.querySelector("#link-form").classList.remove("hide");
+
     const input = document.querySelector("#message-input");
     const encrypted = btoa(input.value);
 
